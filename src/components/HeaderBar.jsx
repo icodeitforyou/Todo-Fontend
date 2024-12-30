f64d08a73c555e61d0f7d94503126441c3550a1d
riturainycity <ritu@rainycityagency.com>
2024-12-30 22:09:32 +0530

    xyz

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-export default function Header() {
+export default function HeaderBar() {
   const token = localStorage.getItem("token");
   const navigate = useNavigate();
   return (
