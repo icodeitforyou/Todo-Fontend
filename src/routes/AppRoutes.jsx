a4f067c785467f612de98b1dff67b6d9bf2bac92
riturainycity <ritu@rainycityagency.com>
2024-12-30 22:20:24 +0530

    fixes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import SignIn from "../components/auth/signin";
-import SignUp from "../components/auth/signup";
+
 import AuthLayout from "../components/AuthLayout/AuthLayout";
 import Footer from "../components/Footer";
 import HeroSection from "../components/HeroSection";
 import NotFound from "../components/NotFound";
 import ToDoList from "../components/ToDoList";
 import Profile from "../components/Profile";
-import HeaderBar from "../components/HeaderBar";
+import SignIn from "../components/auth/SignIn";
+import SignUp from "../components/auth/SignUp";
 
 export default function AppRoutes() {
   return (
     <>
-      <HeaderBar />
+      <Header />
       <Routes>
         <Route index element={<HeroSection />} />
         <Route path="/signin" element={<AuthLayout children={<SignIn />} />} />
